d3fb44d12a0830fe59252e0075ad8c0ca6599a80
snaybanban <36524932+snaybanban@users.noreply.github.com>
2021-03-25 11:55:40 -0600

    Validación de usuario

--- application.py
+++ application.py
@@ -16,18 +16,15 @@ if not ("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
-# Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+
 # Set up database
 engine = create_engine("postgres://ajigwrxxtsfjgh:eef2bb35cd85a37e7d10b5699794a552f7dd4736560b44c88126e9f1228f8f51@ec2-54-145-102-149.compute-1.amazonaws.com:5432/d8tvv4bh4q66pr")
+#engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))  
 
 @app.route("/")
@@ -63,13 +60,13 @@ def login():
         print(rows)
         ####check contraseña
         if rows == None or not check_password_hash(rows[2], request.form.get("password")):
-            print('error: algo malo esta pasando')
+            print('error: contraseña incorrecta o usuario no registrado')
             return render_template("error.html")
        
         # Redirect user to home page
         print(rows[1])
-        session["user_id"] = rows[1]
-        return redirect(url_for("index"))
+        session["user_id"] = rows[0]
+        return render_template("index.html")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
@@ -85,32 +82,39 @@ def change_password():
         # Ensure current password is not empty
         if not request.form.get("password"):
             flash("ALGO SALIO MAL")
-
+            return render_template("change_password.html")
         # Query database for user_id
-        rows=db.execute("SELECT * FROM users WHERE username=:username", {"username":request.form.get("username")})
-
+        rows=db.execute("SELECT * FROM users WHERE id_user = :user", {"user":session["user_id"]}).fetchall()
+        print(rows)
         # Ensure current password is correct
         if rows == None or not check_password_hash(rows[2], request.form.get("password")):
             flash("ALGO SALIO MAL")
+            return render_template("change_password.html")
 
         # Ensure new password is not empty
         if not request.form.get("new_password"):
             flash("ALGO SALIO MAL")
+            return render_template("change_password.html")
 
         # Ensure new password confirmation is not empty
         elif not request.form.get("new_password_confirmation"):
             flash("ALGO SALIO MAL")
+            return render_template("change_password.html")
 
         # Ensure new password and confirmation match
         elif request.form.get("new_password") != request.form.get("new_password_confirmation"):
             flash("ALGO SALIO MAL")
+            return render_template("change_password.html")
+        else:
+            # Update database
+            hashs = generate_password_hash(request.form.get("new_password"))
+            rows = db.execute("UPDATE users SET hashs = :hashs WHERE user_id = :user_id", user_id=session["user_id"], hashs=hashs)
+            db.commit()
+            session.clear()
 
-        # Update database
-        hash = generate_password_hash(request.form.get("new_password"))
-        rows = db.execute("UPDATE users SET hash = :hash WHERE id = :user_id", user_id=session["user_id"], hash=hash)
-
-        # Show flash
-        flash("Changed!")
+            # Show flash
+            flash("Changed!")
+            return render_template("login.html")
 
     return render_template("change_password.html")
 
@@ -135,26 +139,34 @@ def register():
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            flash("VACIO")
+            flash("username vacio")
             return render_template("register.html")
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            flash("VACIO")
+            flash("password vacio")
             return render_template("register.html")
 
         # Ensure password and confirmation match
         elif not request.form.get("password") == request.form.get("confirmation"):
             flash("No coincide")
             return render_template("register.html")
-        {hash==generate_password_hash(request.form.get("confirmation"))}
 
-        # Display a flash message
-        flash("Registered!")
+        # hash the password and insert a new user in the database
+        paswordhash = generate_password_hash(request.form.get("confirmation"))
+        rows = db.execute("INSERT INTO users (username, hashs) VALUES(:username, :paswordhash)",{"username":request.form.get("username"), "paswordhash":paswordhash})
 
-        # Redirect user to home page
-        return redirect(url_for("index"))
+        db.commit()
 
+        return render_template("login.html")
+        
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("register.html")
+
+@app.route("/busqueda", methods=["GET", "POST"])
+def busqueda():
+    
+    if request.method == "POST":
+
+        return redirect(url_for("busqueda"))
